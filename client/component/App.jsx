28af5c29e2183839f2ef5949ca19aa1a7787a294
Duy Mai <duymai1612@gmail.com>
2017-11-13 16:57:37 +0700

    without lists.push

--- client/component/App.jsx
+++ client/component/App.jsx
@@ -24,7 +24,7 @@ function NotSingle() {
     <div>
       <div>
         <span> His/Her Name: </span>
-        <input type="text"/>
+        <input type="text" />
       </div>
       <div>
         <span> First Day Together: </span>
@@ -45,22 +45,45 @@ function AdditionalForm(props) {
   return null
 }
 
+function CreateTable(props) {
+  if (!props.hadTable) return null;
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">Marital Status</th>
+          <th scope="col">First Day Together</th>
+          <th scope="col">Last Day Together</th>
+          <th scope="col">His/Her Name</th>
+        </tr>
+      </thead>
+    </table>
+  )
+}
+
+let lists = [];
+
+
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: "single"
+      selectValue: "single",
+      hadTable: false
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value})
+    this.setState({selectValue: event.target.value})
   }
 
-  handleSubmit(event) {
-
+  handleSubmit() {
+    if (!this.state.hadTable) {
+      this.setState({hadTable: true})
+    }
   }
 
   render() {
@@ -69,49 +92,33 @@ export default class App extends React.Component {
         <h1>Hello World</h1>
         <div>
           <span> Name: </span>
-          <input type="text" id="type-name"/>
+          <input type="text" id="type-name" />
         </div>
         <div>
           <span> Age: </span>
-          <input type="text" id="type-age"/>
+          <input type="text" id="type-age" />
         </div>
         <div>
           <span> Gender: </span>
-          <input type="radio" name="gender" id="male"/>
+          <input type="radio" name="gender" id="male" />
           <label> Male </label>
-          <input type="radio" name="gender" id="female"/> 
+          <input type="radio" name="gender" id="female" /> 
           <label> Female </label>
         </div>
         <div>
           <span> Marital Status </span>
-          <select value={this.state.value} onChange={this.handleChange}>
+          <select value={this.state.selectValue} onChange={this.handleChange}>
             <option value="single">Single</option>
             <option value="not single">Married/In a relationship</option>
             <option value="just broke up">Just broke up</option>
           </select>
         </div>
-        <AdditionalForm value={this.state.value} />
-        <button>
+        <AdditionalForm value={this.state.selectValue} />
+        <button onClick={this.handleSubmit}>
           Submit 
         </button>
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">Marital Status</th>
-              <th scope="col">First Day Together</th>
-              <th scope="col">Last Day Together</th>
-              <th scope="col">His/Her Name</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>fsd</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-          </tbody>
-      </table>
+        <CreateTable hadTable={this.state.hadTable} />
+
       </div>
     );
   }
